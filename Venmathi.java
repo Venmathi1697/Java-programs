176c83ccb150f63ed55b09a1cb5eb8aebd62ca6c
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-20 22:57:33 +0530

    Abstraction example

---
+++ Venmathi.java
@@ -0,0 +1,18 @@
+//abstraction example
+
+public abstract class Venmathi
+{
+public static void main(String[] args)
+{
+
+}
+public abstract void officeAutomation();
+public void dancing()
+{
+System.out.println("Dancing for folk dance");
+}
+public void singing()
+{
+System.out.println("Singing for melody");
+}
+}
---
+++ Vicky.java
@@ -0,0 +1,25 @@
+//abstraction example 
+
+
+public class Vicky extends Venmathi
+{
+public static void main(String[] args)
+{
+Vicky v = new Vicky();
+v.officeAutomation();
+v.dancing();
+v.singing();
+}
+public void officeAutomation()
+{
+System.out.println("course completed");
+}
+public void dancing()
+{
+System.out.println("dancing for western");
+}
+public void singing()
+{
+System.out.println("Singing for rock ");
+}
+}
