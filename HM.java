917b783e5af28ed28e3b0500f1197997992dbb9a
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-21 22:29:08 +0530

    simple method calling program

---
+++ HM.java
@@ -0,0 +1,16 @@
+public class HM
+{
+public static void main(String[] args)
+{
+HM h=new HM();
+h.rounding();
+}
+void rounding()
+{
+System.out.println("The headmaster");
+System.out.println("doing duty");
+System.out.println("managing the school");
+System.out.println("conduct meeting");
+System.out.println("responsibility of the school");
+}
+}
