be462b0466fdc7fe6c7563619edc70c3320e6c59
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-23 22:24:50 +0530

    constructor overloading

---
+++ Fruitshop.java
@@ -0,0 +1,30 @@
+public class Fruitshop
+{
+int price;
+int kg;
+String fruitsname;
+String customername;
+
+public Fruitshop()
+{
+price=100;
+kg=1;
+fruitsname="mango";
+customername="Ram";
+
+
+
+}
+public static void main(String[] args)
+	{
+	Fruitshop f=new Fruitshop();
+	System.out.println(f.price);
+System.out.println(f.kg);
+System.out.println(f.fruitsname);
+System.out.println(f.customername);		
+		
+	}		
+	
+
+
+}
