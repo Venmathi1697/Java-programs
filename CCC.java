93ac768f9cd8842ae49b76c67d1ba8787ec0c97b
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-24 22:42:16 +0530

    single inheritance

---
+++ CCC.java
@@ -0,0 +1,11 @@
+public class CCC
+{
+public void methodc()
+{
+System.out.println("Single inheritance contains one parent one child");
+System.out.println("Multiple inheritance does not exits in java");
+System.out.println("multilevel contains one parent and two or more child");
+}
+
+
+}
