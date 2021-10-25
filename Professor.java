7c6bfacac603d44c53842f9636bf324d3fd54291
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-25 22:08:51 +0530

    simple method calling

---
+++ Professor.java
@@ -0,0 +1,18 @@
+public class Professor
+{
+public static void main(String[] args)
+{
+Professor p=new Professor();
+p.teaching();
+}
+void teaching()
+{
+System.out.println("teaching Maths");
+System.out.println("teaching logistics");
+System.out.println("teaching java");
+System.out.println("teaching computer science");
+System.out.println("teching aptitude");
+
+
+}
+}
