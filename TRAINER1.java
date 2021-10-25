b2f39f15e153e53338d640e88e71d50685650b09
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-25 22:09:36 +0530

    single Inheritance

---
+++ TRAINER1.java
@@ -0,0 +1,23 @@
+class TRAINER
+{
+
+void Training()
+{
+System.out.println("Training java");
+}
+}
+public class TRAINER1 extends TRAINER
+{
+
+void Training()
+{
+System.out.println("Training for aptitude");
+}
+public static void main(String[] args)
+{
+TRAINER t=new TRAINER();
+t.Training();
+}	
+
+}
+
