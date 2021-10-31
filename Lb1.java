8d39f06ce0bc0aa0fa2d5e7194a33d54e3fd6f72
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-31 19:33:31 +0530

    linear search

---
+++ Lb1.java
@@ -0,0 +1,21 @@
+package LinearBinary;
+
+public class Lb1 {
+public static void main(String[] args)
+{
+int[] ar= {10,20,5,8,25};
+int no=5;
+for(int i=0;i<ar.length;i++)
+{
+	if(no==ar[i])
+	{
+		System.out.println("found numbers");
+		break;
+	}
+}
+	
+	
+	
+	
+}
+}
