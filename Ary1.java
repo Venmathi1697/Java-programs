4f5fe9102e38af09f99f9014bcbd37e5674348ff
venmathi <90121932+Venmathi1697@users.noreply.github.com>
2021-10-31 19:31:52 +0530

    Arrays practice problems

---
+++ Ary1.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Ary1 {
+public static void main (String[] args)
+{
+int[] t= {2,4,5,6,7};
+boolean[] result= {true,false,true};
+char[] names= {'a','b','c','d','e'};
+String[] name= {"mathi","vicky","yogesh"};
+for(int i=0;i<names.length;i++)
+System.out.println(names[i]);	
+
+}
+
+
+
+
+
+
+}
---
+++ Ary2.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Ary2 {
+public static void main(String[] args)
+{
+int[] marks=new int[50];
+marks[5]=480;	
+marks[7]=500;
+marks[3]=750;
+marks[4]=900;
+marks[8]=600;
+System.out.println(marks[5]);
+}
+
+
+
+
+
+}
